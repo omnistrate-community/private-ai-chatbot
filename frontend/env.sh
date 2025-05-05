99ddae928f4f7f0fd414e41a8ac01ca39c53b62b
Alok Nikhil <alok@omnistrate.com>
2025-05-05 16:04:58 -0700

    Update to use production build of the Frontend

---
+++ frontend/env.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -Ex
+
+function apply_patch {
+
+    echo "Check that we have NEXT_PUBLIC_API_BASE_URL vars"
+    test -n "$NEXT_PUBLIC_API_BASE_URL"
+
+    find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_API_BASE_URL#$NEXT_PUBLIC_API_BASE_URL#g"
+}
+
+apply_patch
+echo "Starting Nextjs"
+exec "$@"
